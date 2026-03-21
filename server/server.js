a3ddb0688543295125ba3c0623db4abfc50faffb
mpomahac25 <mpomahac25@students.oamk.fi>
2026-03-21 14:00:17 +0200

    Added express-session + server overhaul
    Added express-session to project for handing sessions.
    Migrated frontend.js and backend.js logic into single file to fix cookie-related issue.
    Updated "npm run dev" script to run only server.js.

---
+++ server/server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+
+const path = require('path');
+
+const { sunnifyRouter } = require("./routes/sunnifyRouter.js");
+
+const app = express();
+const client = path.join(__dirname, "..", "client");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || "d8f4a7c9e1b3f6a2d0c9b7e5f8a1c3d2",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 1, // ms * s * min * hr * d * etc...
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    }
+}));
+
+app.use(express.static(client));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(client, "main.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(client, "register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(client, "login.html"));
+});
+
+app.get("/post/create", (req, res) => {
+    res.sendFile(path.join(client, "createpost.html"));
+});
+
+app.use("/", sunnifyRouter);
+
+const port = process.env.BACKEND_PORT;
+
+app.listen(port, () => {
+    console.log(`Running backend on localhost:${port}`);
+});
